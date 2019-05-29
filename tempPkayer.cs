1ea309d9a57977ac46b3e1106a853b91fe042e56
Zulusew <50808317+Zulusew@users.noreply.github.com>
2019-05-29 07:17:35 +0300

    Add files via upload

---
+++ tempPkayer.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempPkayer : MonoBehaviour
+{
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    private float swipeAngle;
+    private tempProbnic tempProb;
+    private Vector2 temp_position;
+    private GameObject tempSharik;
+
+    public int row_x;
+    public int column_y;
+    public int TargetX;
+    public int TargetY;
+
+    public int PreviewX;
+    public int PreviewY;
+
+    public bool MatchesAs = false;
+
+    void Start()
+    {
+        tempProb = FindObjectOfType<tempProbnic>();
+
+        row_x = (int)this.transform.position.x;
+        column_y = (int)this.transform.position.y;
+        TargetY = row_x;
+        TargetX = column_y;
+
+        PreviewX = row_x;
+        PreviewY = column_y;
+
+}
+
+    void Update()
+    {
+        FindMatchesAs();
+        if (MatchesAs)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, .2f);
+            //MatchesAs = false;
+        }
+
+        TargetY = column_y;
+        TargetX = row_x;
+
+        if (Mathf.Abs(row_x - transform.position.x) > .1)
+        {
+            temp_position = new Vector2(row_x, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, temp_position, .1f);
+        }
+        else
+        {
+            temp_position = new Vector2(row_x, transform.position.y);
+            transform.position = temp_position;
+
+            tempProb.stena_sharikov[row_x, column_y] = this.gameObject;
+        }
+
+
+        if (Mathf.Abs(column_y - transform.position.y) > .1)
+        {
+            transform.position = new Vector2(transform.position.x, column_y);
+            transform.position = Vector2.Lerp(transform.position, temp_position, .9f);
+        }
+        else
+        {
+            temp_position = new Vector2(transform.position.x, column_y);
+            transform.position = temp_position;
+            tempProb.stena_sharikov[row_x, column_y] = this.gameObject;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(this.name + " - Имя объекта по которуму нажал курсор");
+        
+    }
+
+    void OnMouseUp(){
+        
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+        MovePice();
+        
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI;
+        Debug.Log(swipeAngle + " - указывает под каким углом отрезок клика мыши");
+    }
+
+    void MovePice()
+    {
+        Debug.Log("Вызов функции перемещение шарика - " + row_x +" - "+ column_y);
+
+        if (swipeAngle >= -45 && swipeAngle <= 45 && row_x < tempProb.Width-1)
+        {
+            // Right movement
+            tempSharik = tempProb.stena_sharikov[row_x + 1 , column_y];
+            tempSharik.GetComponent<tempPkayer>().row_x -= 1;
+
+            row_x += 1;
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && row_x > 0)
+        {
+            //Left movement
+            tempSharik = tempProb.stena_sharikov[row_x - 1, column_y];
+            tempSharik.GetComponent<tempPkayer>().row_x += 1;
+
+            row_x -= 1;
+        }
+        else if (swipeAngle <= 135 && swipeAngle > 45 && column_y < tempProb.Height-1 )
+        {
+            //Up movement
+            tempSharik = tempProb.stena_sharikov[row_x, column_y + 1];
+            tempSharik.GetComponent<tempPkayer>().column_y -= 1;
+
+            column_y += 1;
+        }
+        else if (swipeAngle <= -45 && swipeAngle >= -135 && column_y > 0)
+        {
+            //Down movement
+            tempSharik = tempProb.stena_sharikov[row_x, column_y - 1];
+            tempSharik.GetComponent<tempPkayer>().column_y += 1;
+
+            column_y -= 1;
+            
+        }
+
+        StartCoroutine(CheckMoveCo());
+    }
+
+    void FindMatchesAs()
+    {
+        if (row_x > 0 && row_x < tempProb.Width-1)
+        {
+            
+            GameObject LeftSharik_X = tempProb.stena_sharikov[row_x - 1, column_y];
+            GameObject RightSharik_X = tempProb.stena_sharikov[row_x + 1, column_y];
+            if (LeftSharik_X != null && RightSharik_X != null)
+            {
+                if (LeftSharik_X.tag == this.gameObject.tag && RightSharik_X.tag == this.gameObject.tag)
+                {
+                    LeftSharik_X.GetComponent<tempPkayer>().MatchesAs = true;
+                    RightSharik_X.GetComponent<tempPkayer>().MatchesAs = true;
+                    MatchesAs = true;
+                    
+                }
+            }
+        }
+
+        if ( column_y > 0 && column_y < tempProb.Height - 1)
+        {
+
+            GameObject TopSharik_Y = tempProb.stena_sharikov[row_x, column_y + 1];
+            GameObject DownSharik_Y = tempProb.stena_sharikov[row_x,column_y - 1];
+            if (TopSharik_Y != null && DownSharik_Y != null)
+            {
+                if (TopSharik_Y.tag == this.gameObject.tag && DownSharik_Y.tag == this.gameObject.tag)
+                {
+
+                    TopSharik_Y.GetComponent<tempPkayer>().MatchesAs = true;
+                    DownSharik_Y.GetComponent<tempPkayer>().MatchesAs = true;
+                    MatchesAs = true;
+
+                }
+            }
+        }
+
+
+    }
+
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        if (tempSharik != null)
+        {
+           if(!MatchesAs && !tempSharik.GetComponent<tempPkayer>().MatchesAs)
+            {
+                tempSharik.GetComponent<tempPkayer>().column_y = column_y;
+                tempSharik.GetComponent<tempPkayer>().row_x = row_x;
+
+                row_x = PreviewX;
+                column_y = PreviewY;
+            }
+            tempSharik = null;
+        }
+
+
+    }
+}
---
+++ tempProbnic.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempProbnic : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public GameObject[] shariki;
+    public GameObject[,] stena_sharikov;
+
+    void Start()
+    {
+        stena_sharikov = new GameObject[Width, Height];
+
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                int random_sharik = Random.Range(0, shariki.Length);
+                int MaxVariation = 0;
+
+                while (Perebor(i, j, shariki[random_sharik]) && MaxVariation < 100)
+                {
+                    int temp_random_sharik = Random.Range(0, shariki.Length);
+                    if (random_sharik == temp_random_sharik)
+                    {
+                        temp_random_sharik = Random.Range(0, shariki.Length);
+                        random_sharik = temp_random_sharik;
+                    }
+                    random_sharik = temp_random_sharik;
+                    MaxVariation++;
+                }
+
+                Vector3 temp_position = new Vector2(i, j);
+                stena_sharikov[i,j] = (GameObject)Instantiate(shariki[random_sharik], temp_position, Quaternion.identity);
+                stena_sharikov[i, j].transform.parent = this.transform;
+                stena_sharikov[i,j].name = "Sharik: " + i + " - " + j;
+
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private bool Perebor(int i, int j, GameObject sharik)
+    {
+        if( i > 1 && j > 1)
+        {
+            if (stena_sharikov[i - 1,j].tag == sharik.tag && stena_sharikov[i - 2,j].tag == sharik.tag)
+            {
+                return true;
+            }
+            if (stena_sharikov[i, j - 1].tag == sharik.tag && stena_sharikov[i, j - 2].tag == sharik.tag)
+            {
+                return true;
+            }
+        } else if(i <= 1 || j <= 1)
+        {
+            if (i > 1)
+            {
+                if (stena_sharikov[i -1, j].tag == sharik.tag && stena_sharikov[i -2, j].tag == sharik.tag)
+                {
+                    return true;
+                }
+            }
+            if (j > 1)
+            {
+                if (stena_sharikov[i, j - 1].tag == sharik.tag && stena_sharikov[i, j - 2].tag == sharik.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+
+        return false;
+    }
+
+    private void DestroyMatchesAt(int row_x, int column_y)
+    {
+        if (stena_sharikov[row_x, column_y].GetComponent<tempPkayer>().MatchesAs)
+        {
+            Destroy(stena_sharikov[row_x, column_y]);
+            stena_sharikov[row_x, column_y] = null;
+        }
+    }
+
+    private void DestroyShariki()
+    {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                if (stena_sharikov[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+            }
+        }
+    }
+
+    public IEnumerator DescrewRowCo()
+    {
+        int NullCount = 0;
+
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                if(stena_sharikov[i,j] == null)
+                {
+                    NullCount++;
+                    
+                }
+                if(NullCount > 0)
+                {
+                    stena_sharikov[i, j].GetComponent<tempPkayer>().row_x -= NullCount;
+                }
+            }
+
+            NullCount = 0;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+    }
+}
